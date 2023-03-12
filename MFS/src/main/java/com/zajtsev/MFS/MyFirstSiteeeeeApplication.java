d71fa93616c8d66216158a271a464294686cb715
Trolololek <127530813+Trolololek@users.noreply.github.com>
2023-03-12 23:03:29 +0700

    Add files via upload

---
+++ MFS/src/main/java/com/zajtsev/MFS/MyFirstSiteeeeeApplication.java
@@ -0,0 +1,13 @@
+package com.zajtsev.MFS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSiteeeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSiteeeeeApplication.class, args);
+	}
+
+}
---
+++ MFS/src/test/java/com/zajtsev/MFS/MyFirstSiteeeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zajtsev.MFS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSiteeeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
